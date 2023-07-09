9a8022f42649cc53de09c76b402b0f5f38b78f2f
Hussain Moulana <halihussain616@gmail.com>
2023-07-09 14:39:46 +0100

    dw

--- index.php
+++ index.php
@@ -241,7 +241,7 @@ function decreaseValue() {
             <a href="https://www.instagram.com/themarvelvsdc/?hl=en" target="_blank"> <em class="fa fa-instagram" style="color:magenta; font-size:40px"></em> </a> 
             <a href="https://twitter.com/TheMarvelvsDC?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="_blank"><em class="fa fa-twitter-square" style="color:#00acee; font-size:40px" ></em> </a>  
             </a>  <br>
-             &copy;2021-2025 copyright by Proxy<br>
+             &copy;2021-2025 copyright by Nixon<br>
              all rights reserved.<br>
                 Designed by Hussain Moulana!  
         </div>
